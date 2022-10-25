4e1933c6f76ce08b7aeeb41d3cf298e8b7216f6e
Le Vivilet <levivlet@pm.me>
2022-10-25 14:18:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Language Basics Sql
+
+## Contributing
+
+```sh
+git clone git@github.com:lvce-editor/language-basics-sql.git &&
+cd language-basics-sql &&
+npm ci &&
+npm test
+```
+
+## Gitpod
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/lvce-editor/language-basics-sql)
---
+++ src/tokenizeSql.js
@@ -0,0 +1,119 @@
+/**
+ * @enum number
+ */
+export const State = {
+  TopLevelContent: 1,
+  InsideLineComment: 2,
+  AfterPropertyName: 3,
+  AfterPropertyNameAfterColon: 4,
+  AfterDash: 5,
+  AfterPropertyNameAfterColonAfterNewLine: 6,
+  AfterPipe: 7,
+  InsideMultiLineString: 8,
+  InsideMultiLineStringAfterWhitespace: 9,
+  AfterPropertyValue: 10,
+  InsidePropertyNameStringSingleQuoted: 11,
+  InsidePropertyNameStringDoubleQuoted: 12,
+}
+
+export const StateMap = {
+  [State.TopLevelContent]: 'TopLevelContent',
+  [State.InsideLineComment]: 'InsideLineComment',
+}
+
+/**
+ * @enum number
+ */
+export const TokenType = {
+  Function: 3,
+}
+
+export const TokenMap = {
+  [TokenType.Function]: 'Function',
+}
+
+const RE_LINE_COMMENT_START = /^#/
+const RE_WHITESPACE = /^ +/
+const RE_CURLY_OPEN = /^\{/
+const RE_CURLY_CLOSE = /^\}/
+const RE_PROPERTY_NAME = /^[\p{L}\p{P}\p{S}.\d]+(?=\s*:(\s+|$))/u
+const RE_PROPERTY_NAME_2 =
+  /^[\:@\/\\a-zA-Z\-\_\d\.\s]*[@\/\\a-zA-Z\-\_\d\.](?=\s*:(\s+|$))/
+const RE_PROPERTY_VALUE_1 = /^[^&].*(?=\s+#)/s
+const RE_SEMICOLON = /^;/
+const RE_COMMA = /^,/
+const RE_ANYTHING_BUT_COMMENT = /^[^#]+/
+const RE_NUMERIC = /^(([0-9]+\.?[0-9]*)|(\.[0-9]+))(?=\s*$|\s+#|\s*[\[\]\{\}])/
+const RE_ANYTHING_UNTIL_CLOSE_BRACE = /^[^\}]+/
+const RE_BLOCK_COMMENT_START = /^\/\*/
+const RE_BLOCK_COMMENT_END = /^\*\//
+const RE_BLOCK_COMMENT_CONTENT = /^.+?(?=\*\/|$)/s
+const RE_ROUND_OPEN = /^\(/
+const RE_ROUND_CLOSE = /^\)/
+const RE_PSEUDO_SELECTOR_CONTENT = /^[^\)]+/
+const RE_SQUARE_OPEN = /^\[/
+const RE_SQUARE_CLOSE = /^\]/
+const RE_ATTRIBUTE_SELECTOR_CONTENT = /^[^\]]+/
+const RE_QUERY = /^@[a-z\-]+/
+const RE_STAR = /^\*/
+const RE_QUERY_NAME = /^[a-z\-]+/
+const RE_QUERY_CONTENT = /^[^\)]+/
+const RE_COMBINATOR = /^[\+\>\~]/
+const RE_LANGUAGE_CONSTANT = /^(?:true|false|null)(?!#)/
+const RE_COLON = /^:/
+const RE_DASH = /^\-/
+const RE_WORDS = /^[\w\s]*\w/
+const RE_KEY_PRE = /^\s*(\-\s*)?/
+const RE_SINGLE_QUOTE = /^'/
+const RE_DOUBLE_QUOTE = /^"/
+const RE_STRING_SINGLE_QUOTE_CONTENT = /^[^']+/
+const RE_STRING_DOUBLE_QUOTE_CONTENT = /^[^"]+/
+const RE_ALIAS = /^\*.+/
+const RE_ANCHOR = /^\&.+/
+const RE_BUILTIN_FUNCTION = /^(?:abs|acos|asin|atan|atn2)\b/i
+const RE_ANYTHING = /^.+/s
+
+export const initialLineState = {
+  state: State.TopLevelContent,
+  tokens: [],
+}
+
+export const hasArrayReturn = true
+
+/**
+ * @param {string} line
+ * @param {any} lineState
+ */
+export const tokenizeLine = (line, lineState) => {
+  let next = null
+  let index = 0
+  let tokens = []
+  let token = TokenType.None
+  let state = lineState.state
+  while (index < line.length) {
+    const part = line.slice(index)
+    switch (state) {
+      case State.TopLevelContent:
+        if ((next = part.match(RE_BUILTIN_FUNCTION))) {
+          token = TokenType.Function
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_ANYTHING))) {
+          token = TokenType.Text
+          state = State.TopLevelContent
+        } else {
+          part //?
+          throw new Error('no')
+        }
+        break
+      default:
+        throw new Error('no')
+    }
+    const tokenLength = next[0].length
+    index += tokenLength
+    tokens.push(token, tokenLength)
+  }
+  return {
+    state,
+    tokens,
+  }
+}
